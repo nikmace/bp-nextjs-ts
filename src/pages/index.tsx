3df1b0b5c6249e7bebcc7a015db0b9adca1599b8
nikmace <nikmacentosh@gmail.com>
2022-08-30 10:10:31 +0300

    Added trpc hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
+  const { mutate } = trpc.useMutation(['some.mutation-here']);
+
   return (
     <div className="text-3xl header">
       <h1>Hello from NextJS</h1>
